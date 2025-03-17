2a8c82e9cb01c261fcd951f942b5c3046d672c87
TrevorMcCormack <mccormackt746@gmail.com>
2025-03-17 15:47:19 -0400

    styling

--- topic-05-control-structures/evaluator.py
+++ topic-05-control-structures/evaluator.py
@@ -11,15 +11,18 @@ def evaluate(ast, environment={}):
             value = evaluate(statement, environment)
             last_value = value
         return last_value
+    
     if ast["tag"] == "block":
         for statement in ast["statements"]:
             _ = evaluate(statement, environment)
+
     if ast["tag"] == "print":
         value = evaluate(ast["value"], environment)
         s = str(value)
         print(s)
         printed_string = s
         return None
+    
     if ast["tag"] == "if":
         condition_value = evaluate(ast["condition"], environment)
         if condition_value:
@@ -28,10 +31,12 @@ def evaluate(ast, environment={}):
             if ast["else"]:
                 evaluate(ast["else"], environment)
         return None
+    
     if ast["tag"] == "while":
         while evaluate(ast["condition"], environment):
             evaluate(ast["do"], environment)
         return None
+    
     if ast["tag"] == "assign":
         target = ast["target"]
         assert target["tag"] == "identifier"
@@ -39,20 +44,25 @@ def evaluate(ast, environment={}):
         assert type(identifier) is str
         value = evaluate(ast["value"],environment)
         environment[identifier] = value
+
     if ast["tag"] == "number":
         return ast["value"]
+    
     if ast["tag"] == "identifier":
         if ast["value"] in environment:
             return environment[ast["value"]]
         parent_environment = environment
+
         while "$parent" in parent_environment:
             parent_environment = environment["$parent"]
             if ast["value"] in parent_environment:
                 return parent_environment[ast["value"]]
         raise Exception(f"Value [{ast["value"]}] not found in environment {environment}.")
+    
     if ast["tag"] in ["+", "-", "*", "/"]:
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
+
         if ast["tag"] == "+":
             return left_value + right_value
         if ast["tag"] == "-":
@@ -61,40 +71,50 @@ def evaluate(ast, environment={}):
             return left_value * right_value
         if ast["tag"] == "/":
             return left_value / right_value
+        
     if ast["tag"] == "negate":
         value = evaluate(ast["value"], environment)
         return -value
+    
     if ast["tag"] == "&&":
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
         return left_value and right_value
+    
     if ast["tag"] == "||":
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
         return left_value or right_value
+    
     if ast["tag"] == "!":
         value = evaluate(ast["value"], environment)
         return not value
+    
     if ast["tag"] == "<":
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
         return left_value < right_value
+    
     if ast["tag"] == ">":
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
         return left_value > right_value
+    
     if ast["tag"] == "<=":
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
         return left_value <= right_value
+    
     if ast["tag"] == ">=":
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
         return left_value >= right_value
+    
     if ast["tag"] == "==":
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
         return left_value == right_value
+    
     if ast["tag"] == "!=":
         left_value = evaluate(ast["left"], environment)
         right_value = evaluate(ast["right"], environment)
@@ -191,6 +211,7 @@ def test_evaluate_identifier():
         raise Exception("Error expected for missing value in environment")
     except Exception as e:
         assert "not found" in str(e) 
+    
     assert eval("x+3", {"x":3}) == 6
     assert eval("x+y",{"x":4,"y":5}) == 9
     assert eval("x+y",{"$parent":{"x":4},"y":5}) == 9
